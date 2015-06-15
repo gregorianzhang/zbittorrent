25079736594b0fc5c992503891b2b8ce94aadc88
Roy <gregorian.zhang@gmail.com>
2015-06-16 01:44:10 +0800

    string num dict

--- ben.py
+++ ben.py
@@ -27,15 +27,51 @@ class ben(object):
                 return(num,len(num)+2)
 
     def ben_dict(self,data):
-        dic = {}
-        
+        dict1 = {}
+        n=1
+	print "data[%s] is value %s" % (n,data)
+	while data[n] != 'e':
+	    print "dict1 %r" % dict1
+	    print "data[%s] is value %s" % (n,data[n])
+	    #key
+	    if data[n].isdigit():
+	        (z,i) = self.ben_string(data[n:])
+		print z,i
+	    else:
+	    	if data[n] == 'i':
+	            (z,i) = self.ben_num(data[n:])
+	    print "z %r i %r" % (z,i)    
+	    print "1" *40
+	    n += i
+	    #value
+	    print "data[%s] is value %s" % (n,data[n])
+	    if data[n].isdigit():
+		(v,t) = self.ben_string(data[n:])
+	    else:
+	    	if data[n] == 'i':
+		    (v,t) = self.ben_num(data[n:])
+	    n += t
+	
+	    print "z %s,v %s" % (z,v)	
+	    dict1[z]=v
+
+	    #if n > num:
+#		return dict1
+		
+	return dict1
+	
 
 if __name__ == "__main__":
     a = ben()
     x="2:aa2:ab2:ac13:qazxswedcvf5r"
     c="i233334ei32e"
     d="i-32e"
+    tt="d2:aa2:abe"
+    td="d4:aaaai333e2:445:abcde4:xyzzi4356ee"
     #a.decode(x)
     print a.ben_string(x)
     print a.ben_num(c)
     print a.ben_num(d)
+    print "*" * 30
+    print a.ben_dict(tt)
+    print a.ben_dict(td)
